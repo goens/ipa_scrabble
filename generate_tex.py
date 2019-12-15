447156a1ae6a858d3d3037b758e5459904a05fca
Andres Goens <goens@goenshome>
2019-12-15 23:22:13 +0100

    first prototype

--- generate_tex.py
+++ generate_tex.py
@@ -1,9 +1,15 @@
+MATRIX_LENGTH=10
 import csv
 
-def generate_tile(label,end_row=False):
+def generate_tile(label,score,end_row=False):
    text = "\\node[tile,label=center:\\Huge "
    if label != 'BLANK':
        text += label
+       text += ",label={[xshift=5mm, yshift=-15mm]"
+       text += str(score)
+       text += '}'
+   else:
+       text += ' '
    text += "] {}; "
    if end_row:
        return (text + "\\\\")
@@ -13,11 +19,24 @@ def generate_tile(label,end_row=False):
 
 
 letters_dict = {}
+length = 0
 with open("frequency_chart.csv",'r') as filehandle:
     reader = csv.DictReader(filehandle)
     for row in reader:
-        letters_dict[row['IPA']] = { 'frequency' : row['How many to make'], 'score' : row['Score']}
 
-#with open('tilematrix.tex','w') as tilefile:
-for letter in letters_dict:
-    print(generate_tile(letter))
+        letters_dict[row['IPA']] = { 'frequency' : int(row['How many to make']), 'score' : int(row['Score'])}
+        length += int(row['How many to make'])
+
+with open('tilematrix.tex','w') as tilefile:
+    idx = 0
+    total = 0
+    for letter in letters_dict:
+        for _ in range(int(letters_dict[letter]['frequency'])):
+            idx += 1
+            total += 1
+            if idx == MATRIX_LENGTH or total == length:
+                end_row = True
+                idx = 0
+            else:
+                end_row = False
+            tilefile.write(generate_tile(letter,letters_dict[letter]['score'],end_row=end_row) + '\n')
